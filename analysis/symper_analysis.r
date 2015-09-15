3f6398e39d4dfc46ae7d6bf93f02953a67a8e7ea
Jeremy Cole <jrc436@psu.edu>
2015-09-15 15:44:19 -0400

    adding mathpsych dir

--- analysis/symper_analysis.r
+++ analysis/symper_analysis.r
@@ -1,3 +1,4 @@
 library(lme4)
-table <- read.csv(file="C:/work/home/sym-perception/analysis/symper_results.csv") 
-model <- lmer(accuracy ~ rot_same + ref_same + gref_same + tile_same + (1|user_id) + (1|task_id) + distance, data = table)
+table <- read.csv(file="/work/research/symper/analysis/symper_results.csv") 
+#model <- lmer(accuracy ~ rot_same + ref_same + gref_same + tile_same + (1|user_id) + (1|task_id) + distance, data = table)
+model <- lmer(accuracy ~ ref_same + gref_same + tile_same + rot_same + distance + (1|user_id) + (1|task_id), data=table)
