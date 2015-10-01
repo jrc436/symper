82b3539ce3b7a9649a60b0653f96eb37b26c8bbc
Jeremy Cole <jrc436@psu.edu>
2015-10-01 16:01:25 -0400

    added paper stuff

--- analysis/symper_analysis.r
+++ analysis/symper_analysis.r
@@ -1,4 +1,9 @@
 library(lme4)
-table <- read.csv(file="/work/research/symper/analysis/symper_results.csv") 
-#model <- lmer(accuracy ~ rot_same + ref_same + gref_same + tile_same + (1|user_id) + (1|task_id) + distance, data = table)
-model <- lmer(accuracy ~ ref_same + gref_same + tile_same + rot_same + distance + (1|user_id) + (1|task_id), data=table)
+table <- read.csv(file="/work/research/symper/analysis/symper_results.csv")
+table_rot <- read.csv(file="/work/research/symper/analysis/symper_rot_results.csv")
+table_refl <- read.csv(file="/work/research/symper/analysis/symper_ref_results.csv")
+model_summary <- lmer(accuracy ~ rot_same + ref_same + gref_same + tile_same + (1|user_id) + (1|task_id) + distance, data = table)
+model_distance <- lmer(accuracy ~ distance + (1|user_id) + (1|task_id), data=table)
+model_rot <- lmer(accuracy ~ same2fold + same3fold + same4fold + same6fold + (1|user_id) + (1|task_id), data=table_rot)
+model_ref <- lmer(accuracy ~ T1_same + T2_same + D1_same + D2_same + (1|user_id) + (1|task_id), data=table_refl)
+
